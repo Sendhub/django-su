371b12035fa71b030f7e7810120b537673e7e711
lkumar-sendhub <lkumar@sendhub.com>
2021-06-21 12:19:15 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ from distutils.core import setup
 VERSION = "0.9.0"
 
 setup(
-    name="django-su",
+    name="django_su",
     version=VERSION,
     description="child of django-su package(django's)",
     author="Django, Updated by Lalithkumar",
     url='https://sendhub.com/',
-    packages=['django-su'],
+    packages=['django_su'],
     package_data={'sendhub': [VERSION]}
 )
