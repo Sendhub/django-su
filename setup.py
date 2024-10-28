ad8e1545ca98f23fcd852231417a22bea76beb86
Ron Peters <rpeters@sendhub.com>
2024-10-28 11:03:08 -0230

    updating for 3.11

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ setup.py
 """
 from distutils.core import setup
 
-VERSION = "0.9.0"
+VERSION = "0.9.1"
 
 setup(
     name="django_su",
     version=VERSION,
     description="child of django-su package(django's)",
-    author="Django, Updated by Lalithkumar",
+    author="Django, Updated by RPeters",
     url='https://sendhub.com/',
     packages=['django_su'],
     package_data={'sendhub': [VERSION]}
